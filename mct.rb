a658a5f0f7aef2b6bcd0d3a7d997888a08b7fef8
SetScript <101998415+SetScript@users.noreply.github.com>
2023-05-27 00:57:29 +0300

    update main file

--- mct.rb
+++ mct.rb
@@ -1,5 +1,15 @@
 require_relative 'src/class.rb'
+require_relative 'src/function.rb'
 
-data = ARGV[0]
+data0 = ARGV[0]
+@data = ARGV[1]
 
-puts ("#{data}").byte
+if data0 == "stm"
+    puts ("#{data}").byte
+elsif data0 == "mts"
+    unbyte()
+elsif data0 == 'help'
+    puts "MCT(Machine Code Transaltor)\n\nCommands:\n   -stm[EXAMPLE: mct.rb stm <Your string here>]\n      -translates a string into machine code\n    -mts[EXAMPLES: mct.rb mts <machine code here>]\n       -translates machine code into a string\n    -help\n       -shows this text\n\nIMPORTANT:\n     -in order for the command to be correctly recognized, you need to write them in lowercase"
+else
+    puts "mct: '#{data0}' is not a mct command. See 'mct.rb help'."
+end
